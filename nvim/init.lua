ae8493558d605cfa02711ec911ac8819de091c6e
Simone <iannisimo@gmail.com>
2024-09-04 15:02:47 +0200

    nerdtree config

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,7 +40,7 @@ opt.nrformats = 'bin,hex' -- 'octal'
 opt.undofile = true
 opt.splitright = true
 opt.splitbelow = true
-opt.cmdheight = 0
+opt.cmdheight = 1
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
--- nvim/plugin/nerdtree.lua
+++ nvim/plugin/nerdtree.lua
@@ -1,12 +1,12 @@
--- nnoremap <leader>n :NERDTreeFocus<CR>
--- nnoremap <C-n> :NERDTree<CR>
--- nnoremap <C-t> :NERDTreeToggle<CR>
--- nnoremap <C-f> :NERDTreeFind<CR>
-
 local keymap = vim.keymap
 
-keymap.set('n', '<space>no', vim.cmd.NEDRTree, { desc = '[n]erdtree: [o]pen' })
--- keymap.set('n', '')
--- keymap.set('n', '')
--- keymap.set('n', '')
--- keymap.set('n', '')
+keymap.set('n', '<space>no', vim.cmd.NERDTree, { desc = '[n]erdtree: [o]pen' })
+keymap.set('n', '<space>nf', vim.cmd.NERDTreeFocus, { desc = '[n]erdtree: [f]ocus' })
+keymap.set('n', '<space>nt', vim.cmd.NERDTreeToggle, { desc = '[n]erdtree: [t]oggle' })
+keymap.set('n', '<space>nF', vim.cmd.NERDTreeFind, { desc = '[n]erdtree: [F]ind' })
+
+
+vim.api.nvim_create_autocmd('VimEnter', { command = "NERDTree | wincmd p" })
+vim.api.nvim_create_autocmd('BufEnter', { command = 'if tabpagenr("$") == 1 && winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree() | call feedkeys(":quit\n") | endif' })
+
+vim.g.NERDTreeFileLines = 1

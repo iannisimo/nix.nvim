fa2915937ad3ca3c72fa4803d33282aa4b0beef2
Simone <iannisimo@gmail.com>
2024-09-11 15:17:45 +0200

    remote-sshfs -

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -87,7 +87,6 @@ with final.pkgs.lib; let
     # Mine
     nerdtree
     vim-visual-multi
-    ( mkNvimPlugin inputs.remote-sshfs "remote-sshfs" )
   ];
 
   extraPackages = with pkgs; [
--- nvim/plugin/remote-sshfs.lua
+++
@@ -1,9 +0,0 @@
-require('remote-sshfs').setup{
-  connections = {
-    ssh_configs = { -- which ssh configs to parse for hosts list
-      vim.fn.expand "$HOME" .. "/.ssh/config",
-      "/etc/ssh/ssh_config",
-      "/run/agenix/ssh_config"
-    }
-  }
-}
